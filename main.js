16f6d945ea0fddc24bb4918663da8d7a2868c5e6
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-05-24 12:50:47 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// Write your JavaScript code here
+const element = document.querySelector('#error');
+element.classList.add('animate__animated', 'animate__lightSpeedInLeft' );
+
+let Id = document.querySelector('#subtitle');
+Id.style.setProperty('animate-delay', '2s');
+Id.classList.add('animate__flash');
